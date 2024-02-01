368d09e2b014d093b40d00648909f2d2e4066ef8
Shivam Saundale <113767272+saundale@users.noreply.github.com>
2024-02-01 23:25:44 +0530

    Add files via upload

---
+++ src/components/Tours.js
@@ -0,0 +1,20 @@
+import Card from './Card'
+
+function Tours({tours,removeTour}){
+    return(
+        <div className="container">
+            <div>
+            <h2 className="title">Plan with Love</h2>
+            </div>
+            <div className="cards">
+                {
+                    tours.map((tour)=>{
+                        return <Card {...tour} key={tour.id} removeTour={removeTour}></Card>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Tours; 
